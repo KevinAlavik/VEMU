b1d28a4c9043f2cbc2adbfe2d1262d6836176217
puffer <kevin@alavik.se>
2024-03-19 10:13:04 +0100

    Create bus.h

---
+++ src/bus.h
@@ -0,0 +1,21 @@
+#ifndef __BUS_H__
+#define __BUS_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint32_t (*BusReadFunc)(uint16_t);
+typedef void (*BusWriteFunc)(uint16_t, uint32_t);
+
+struct device {
+    uint16_t base;
+    uint16_t limit;
+    BusReadFunc read;
+    BusWriteFunc write;
+};
+bool add_device(uint16_t base, uint16_t limit, BusReadFunc read,
+                BusWriteFunc write);
+void bus_write(uint16_t addr, uint8_t data);
+uint8_t bus_read(uint16_t addr);
+
+#endif // __BUSS_H__
